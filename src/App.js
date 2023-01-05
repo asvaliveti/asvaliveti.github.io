f82371fe34b3eb4d34ee0db6be0e696fcb3c3325
Anirudh Valiveti <asvaliveti@gmail.com>
2023-01-04 17:53:50 -0800

    Configure React app for deployment to GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import Home from './pages/home';
+import Contact from './pages/contact';
+import Involvements from "./pages/involvements";
+import {AppBar, Button, Grid} from "@mui/material";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <AppBar position={"static"} >
+          <Grid container direction={"row"} justifyContent={"center"}>
+            <Grid item>
+              <Button onClick={() => navigate('/involvements')} sx={{color: "#FFFFFF"}}>Involvements</Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={() => navigate('/home')} sx={{color: "#FFFFFF"}}>Home</Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={() => navigate('/contact')} sx={{color: "#FFFFFF"}}>Contact</Button>
+            </Grid>
+          </Grid>
+        </AppBar>
+        <Routes>
+          <Route path="/involvements" element={<Involvements />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </div>
   );
 }
 
---
+++ src/pages/involvements.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Involvements() {
+    return (
+        <h1>Involvements Page</h1>
+    );
+}
+
+export default Involvements;
