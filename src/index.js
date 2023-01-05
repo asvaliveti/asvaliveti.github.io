56d137307482496c424443395f1a46d2afb11223
Anirudh Valiveti <asvaliveti@gmail.com>
2023-01-04 18:46:17 -0800

    plz

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render((
-    <BrowserRouter basename={window.location.pathname}>
-        <App />
+    <BrowserRouter basename={window.location.pathname || ''}>
+        <Route exact path={"/"} component={App}/>
     </BrowserRouter>
 ))
