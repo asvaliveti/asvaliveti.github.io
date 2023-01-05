02c5f8d71d4531a1df5d976ef5f3e7411987e81a
Anirudh Valiveti <asvaliveti@gmail.com>
2023-01-04 18:42:50 -0800

    maybe now

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render((
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname}>
         <App />
     </BrowserRouter>
 ))
