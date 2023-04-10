b1b637cd4817cd2c15463a565388b9dc87b8fb9c
Anirudh Valiveti <asvaliveti@gmail.com>
2023-04-10 16:37:15 -0500

    adding chatbot

--- src/AverageWarriorsFan/startConversation.py
+++
@@ -1,5 +0,0 @@
-import sys
-from chatbot import Chatbot
-
-cb = Chatbot.getInstance()
-cb.inputMessage(sys.argv[1])
--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-const { PythonShell } = require('python-shell');
+import axios from "axios";
 
 class Chatbot extends React.Component {
     constructor(props) {
@@ -9,42 +9,28 @@ class Chatbot extends React.Component {
 
         this.state = {
             messages: [],
-            mostRecentMessage: ""
+            message: "",
+            avgWarriorsFanResponse: "",
         };
     }
 
-    // initChatbotPy() {
-    //     PythonShell.run('../AverageWarriorsFan/chatbot.py', null).then((messages) => {
-    //         console.log('finished');
-    //     });
-    // }
-    //
-    // sendMessagePy(text) {
-    //     let pyshell = new PythonShell('../AverageWarriorsFan/startConversation.py');
-    //     pyshell.send(text);
-    //     pyshell.on('message', function (message) {
-    //         // received a message sent from the Python script (a simple "print" statement)
-    //         console.log(message);
-    //     });
-    //
-    //     pyshell.end(function (err,code,signal) {
-    //         if (err) throw err;
-    //         console.log('The exit code was: ' + code);
-    //         console.log('The exit signal was: ' + signal);
-    //         console.log('finished');
-    //     });
-    // }
-
-    componentDidMount() {
-        // this.initChatbotPy()
-    }
-
-    handleSendMessage = () => {
-        // this.sendMessagePy(this.state.mostRecentMessage);
+    handleSendMessage = async () => {
+        axios.default.baseUrl = ''
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': 'https://average-warriors-fan.herokuapp.com/',
+                'Access-Control-Allow-Methods': "GET, PUT, POST, DELETE, HEAD, OPTIONS",
+            },
+            body: {
+                message: this.state.message
+            }
+        };
+        axios.get('https://asvaliveti.github.io/message', config)
+            .then(response => this.setState({ totalReactPackages: response.data.total }));
     }
 
     handleMessageChange = (e) => {
-        this.setState({mostRecentMessage: e.target.value})
+        this.setState({message: e.target.value})
     }
 
     render() {
